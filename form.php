f87a95c036c855f25f1f264f12a5922e477315e2
Lorenzo Pagani <larrypg2@gmail.com>
2023-12-19 17:59:22 +0100

    feat: adds random password generator

--- form.php
+++ form.php
@@ -10,8 +10,18 @@
 </head>
 
 <body>
-    <header></header>
-    <main></main>
+    <header class="text-center">
+        <h1>Password generator</h1>
+    </header>
+    <main>
+        <div class="container">
+            <form class="form" action="password.php" method="GET">
+                <input class="form-control mb-3" name="numeri" min="1" type="number" required>
+                <button class="btn btn-primary" type="submit">Genera password!</button>
+            </form>
+
+        </div>
+    </main>
     <footer></footer>
     <script src="main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- password.php
+++ password.php
@@ -8,10 +8,40 @@
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+<?php
+$passwordLength = $_GET["numeri"];
+$letters = "qwertyuioiplkjhgfdsazxcvbnm";
+$capsLetters = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+$numbers = "0123456789";
+$symbols = "\%&/()=?'^+*.:<>[]{}-_";
+$lettersArray = str_split($letters, 1);
+$capsLettersArray = str_split($capsLetters, 1);
+$numbersArray = str_split($numbers, 1);
+$symbolsArray = str_split($symbols, 1);
+$charactersArray = array_merge($lettersArray, $capsLettersArray, $numbersArray, $symbolsArray);
+function randomise($length, $array)
+{
+    $result = '';
+    $count = count($array);
+    for ($i = 0; $i < $length; $i++) {
+        $number = rand(0, $count - 1);
+        $result .= $array[$number];
+    }
+    return $result;
+};
+
+
+?>
 
 <body>
     <header></header>
-    <main></main>
+    <main>
+        <h1>
+            <?php
+            echo randomise($passwordLength, $charactersArray);  ?>
+
+        </h1>
+    </main>
     <footer></footer>
     <script src="main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
